ef443f07caf7ffb7930e67c0ef5a05e19797e971
latushko1 <latushko1egor@gmail.com>
2022-09-27 11:21:32 +0600

    add menu-dropdown

--- components/dropdown.js
+++ components/dropdown.js
@@ -10,7 +10,7 @@ function Dropdown() {
     <div>
       <div class="relative inline-block text-left">
         <div>
-        <button onClick={() => setOpen (!open)}  type="button" class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-primary px-4 py-2 text-sm font-bold text-secondary shadow-sm hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">
+        <button onClick={() => setOpen (!open)}  type="button" class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-secondary px-4 py-2 text-sm font-bold text-primary shadow-sm hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">
       Menu
       <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
